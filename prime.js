14521a791fbbabcf70e5d273d77dae7f97144e91
ThepakAhlawat <deepakahlawat2608@gmail.com>
2023-10-25 19:52:15 +0530

    check prime or not

---
+++ prime.js
@@ -0,0 +1,16 @@
+function prime(num){
+    let prime=true;
+  for (let i=2;i<num;i++){
+    if(num%i==1){
+      prime=false;
+      break;
+    }
+  }
+  if(prime==true){
+    return'It is a prime Number'
+  }
+  else{
+    return'It is not a prime Number'
+  }
+  }
+  console.log(prime(101))
